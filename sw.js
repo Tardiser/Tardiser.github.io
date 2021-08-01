bc30f5d6362d930f885ed680e6a9c50589d264da
unknown <erdem_tardis@hotmail.com>
2021-08-01 19:08:03 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5cd0487b69a7fba8b70f.js"
+    "url": "webpack-runtime-8ffd85b66add7b3bf5c7.js"
   },
   {
     "url": "framework-aa35e29fbb74d6100667.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9ae1f2612de5be9bab24126d1d6cdcb2"
+    "revision": "d4aa63525fe8419241155e0fac79c410"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-bafda31d2d983398e3d8.js"
